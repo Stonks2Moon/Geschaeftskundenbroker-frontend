2d3c1a04c29cd382de4bcdaa40f63cc26d2342d8
TristanMrb <trisemig@aol.com>
2021-03-04 11:34:26 +0100

    Added uikit

--- geschaeftskundenbroker-frontend/src/app/app.component.ts
+++ geschaeftskundenbroker-frontend/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+declare var UIkit: any;
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'geschaeftskundenbroker-frontend';
+
+  showAlert(): void {
+    UIkit.modal.alert('UIkit alert!');
+  }
 }
--- geschaeftskundenbroker-frontend/src/app/app.module.ts
+++ geschaeftskundenbroker-frontend/src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './components/nav/nav.component';
+import { FooterComponent } from './components/footer/footer.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
