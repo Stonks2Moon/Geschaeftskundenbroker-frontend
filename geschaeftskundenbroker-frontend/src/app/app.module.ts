a6234aaf41815aa1cf86032f2d38dfed7a6059f5
TristanMrb <trisemig@aol.com>
2021-03-05 18:07:43 +0100

    Added basic components and routing

--- geschaeftskundenbroker-frontend/src/app/app.module.ts
+++ geschaeftskundenbroker-frontend/src/app/app.module.ts
@@ -5,13 +5,27 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './components/nav/nav.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { TradeComponent } from './components/trade/trade.component';
+import { DepotComponent } from './components/depot/depot.component';
+import { HistoryComponent } from './components/history/history.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
-    FooterComponent
+    FooterComponent,
+    TradeComponent,
+    DepotComponent,
+    HistoryComponent,
+    ProfileComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
---
+++ geschaeftskundenbroker-frontend/src/app/components/depot/depot.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-depot',
+  templateUrl: './depot.component.html',
+  styleUrls: ['./depot.component.scss']
+})
+export class DepotComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
