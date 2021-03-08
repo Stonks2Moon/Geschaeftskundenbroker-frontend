ff8002390d47b0df55e5ca5619fc6e628022f701
TristanMrb <trisemig@aol.com>
2021-03-08 14:49:34 +0100

    Added dynamic app name

--- frontend/src/app/components/nav/nav.component.ts
+++ frontend/src/app/components/nav/nav.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Customer } from 'src/app/logic/data-models/data-models';
 import { AuthenticationService } from 'src/app/logic/services/authentication.service';
 
 @Component({
@@ -9,10 +10,15 @@ import { AuthenticationService } from 'src/app/logic/services/authentication.ser
 })
 export class NavComponent implements OnInit {
 
+  public currentCustomer: Customer;
+
   constructor(
     private router: Router,
     private authenticationService: AuthenticationService,
-  ) { }
+  ) {
+
+    this.authenticationService.currentCustomer.subscribe(customer => this.currentCustomer = customer);
+  }
 
   ngOnInit(): void {
   }
