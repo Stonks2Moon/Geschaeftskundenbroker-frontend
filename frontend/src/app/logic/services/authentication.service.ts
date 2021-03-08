a3e0bc4b5ffe3cf0971a7c3f38ef5205ed7435a2
TristanMrb <trisemig@aol.com>
2021-03-08 13:15:24 +0100

    Added environment var and new company api call

--- frontend/src/app/logic/services/authentication.service.ts
+++ frontend/src/app/logic/services/authentication.service.ts
@@ -4,6 +4,7 @@ import { AbstractControl } from '@angular/forms';
 import { CookieService } from 'ngx-cookie-service';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
 import { ControlsMap, Customer, CustomerSession, Login } from '../data-models/data-models';
 
 
@@ -11,23 +12,22 @@ import { ControlsMap, Customer, CustomerSession, Login } from '../data-models/da
   providedIn: 'root'
 })
 export class AuthenticationService {
-
   public isLoggedIn: boolean = false;
+  public currentCustomer: Observable<Customer>;
 
   private currentCustomerSubject: BehaviorSubject<Customer>;
-  public currentCustomer: Observable<Customer>;
+  private apiUrl: string = environment.apiUrl
 
   constructor(private http: HttpClient, private cookieService: CookieService) {
     this.currentCustomerSubject = new BehaviorSubject<Customer>(null);
     this.currentCustomer = this.currentCustomerSubject.asObservable();
-
   }
 
   public signInWithEmailAndPassword(loginValue: ControlsMap<AbstractControl>): Observable<{ session: CustomerSession, customer: Customer }> {
 
     const login: Login = { email: loginValue.email.value, password: loginValue.password.value }
 
-    return this.http.post<{ session: CustomerSession, customer: Customer }>('https://stonks.multiflexxx.de/api/customer/login', { login: login })
+    return this.http.post<{ session: CustomerSession, customer: Customer }>(`${this.apiUrl}customer/login`, { login: login })
       .pipe(
         tap(
           (data) => {
@@ -46,7 +46,7 @@ export class AuthenticationService {
 
   public signInWithSession(session: CustomerSession): Observable<{ session: CustomerSession, customer: Customer }> {
 
-    return this.http.post<{ session: CustomerSession, customer: Customer }>('https://stonks.multiflexxx.de/api/customer/login', { session: session })
+    return this.http.post<{ session: CustomerSession, customer: Customer }>(`${this.apiUrl}customer/login`, { session: session })
       .pipe(
         tap(
           (data) => {
@@ -67,9 +67,7 @@ export class AuthenticationService {
 
     const customer: Customer = { firstName: registerValue.firstName.value, lastName: registerValue.firstName.value, email: registerValue.email.value, password: registerValue.password.value, companyCode: registerValue.companyCode.value }
 
-    console.log(customer);
-
-    return this.http.put<{ session: CustomerSession, customer: Customer }>('https://stonks.multiflexxx.de/api/customer', customer)
+    return this.http.put<{ session: CustomerSession, customer: Customer }>(`${this.apiUrl}customer`, customer)
       .pipe(
         tap(
           (data) => {
--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'https://stonks.multiflexxx.de/api/'
 };
