7b22d4fc70f929dbdfdba979c468a4ef784b2c0f
TristanMrb <trisemig@aol.com>
2021-03-09 12:24:55 +0100

    Added new datamodels

--- frontend/src/app/logic/data-models/data-models.ts
+++ frontend/src/app/logic/data-models/data-models.ts
@@ -3,9 +3,19 @@ export { Address } from './address.model'
 export { ChartValue } from './chart-value.model'
 export { Company } from './company.model'
 export { ControlsMap } from './controls-map.model';
+export { CreateDepot } from './depot/create-depot.model'
 export { CustomerSession } from './customer-session.model'
 export { Customer } from './customer.model'
+export { DepotPosition } from './depot/depot-position.model'
+export { DepotSummary } from './depot/depot-summary.model'
 export { HistoricalData } from './historical-data.model'
 export { Login } from './login.model'
+export { PlaceOrder } from './order/place-order.model'
+export { PlaceShareOrder } from './order/place-share-order.model'
+export { ReturnShareOrder } from './order/return-share-order.model'
 export { Share } from './share.model'
+
+// Export all enums
+export { OrderDetail } from './_enums/order-detail.enum'
+export { OrderType } from './_enums/order-type.enum'
 
