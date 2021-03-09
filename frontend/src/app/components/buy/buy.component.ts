0224e3591a6da37aa49ded42de4d5d7df9a31455
aalenaa <anna-lena.richert@t-online.de>
2021-03-09 15:22:25 +0100

    updated buy component

--- frontend/src/app/components/buy/buy.component.ts
+++ frontend/src/app/components/buy/buy.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { EChartsOption } from 'echarts';
 
 @Component({
   selector: 'app-buy',
@@ -11,5 +12,21 @@ export class BuyComponent implements OnInit {
 
   ngOnInit(): void {
   }
+
+  chartOption: EChartsOption = {
+    xAxis: {
+      type: 'category',
+      data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+    },
+    yAxis: {
+      type: 'value',
+    },
+    series: [
+      {
+        data: [820, 932, 901, 1290, 1330, 1320],
+        type: 'line',
+      },
+    ],
+  };
 
 }
