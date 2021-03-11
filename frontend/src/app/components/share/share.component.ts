9895ab9719ddfe47b7952cdf405f5837ddb6921a
Tony1704 <anton.ochel1@gmail.com>
2021-03-11 12:40:11 +0100

    candlesticks work!

--- frontend/src/app/components/share/share.component.ts
+++ frontend/src/app/components/share/share.component.ts
@@ -45,6 +45,109 @@ export class ShareComponent implements OnInit {
     let x: Array<Date> = [];
     let y: Array<number> = [];
     let data: Array<any> = [];
+    let dates: Array<string> = [];
+
+    //this sorts the data. should not be necessary, would be best if the backend does this
+    this.historicalData.chartValues = this.historicalData.chartValues.sort((n1,n2) => {
+      if (n1.recordedAt > n2.recordedAt) {
+          return 1;
+      }
+  
+      if (n1.recordedAt < n2.recordedAt) {
+          return -1;
+      }
+  
+      return 0;
+  });
+    let currentDate = new Date(this.historicalData.chartValues[0].recordedAt)
+    let open = 0
+    let close = 0
+    let lowest = 0
+    let highest = 0
+    this.historicalData?.chartValues.forEach(element => {
+      let newDate = new Date(element.recordedAt)
+      if(newDate.getFullYear() == currentDate.getFullYear() && newDate.getMonth() == currentDate.getMonth() && newDate.getDay() == currentDate.getDay()){
+        if(open == 0){
+          open = element.recordedValue
+        }
+        close = element.recordedValue
+        if(lowest == 0 || lowest > element.recordedValue){
+          lowest = element.recordedValue
+        }
+        if(highest == 0 || highest < element.recordedValue){
+          highest = element.recordedValue
+        }
+      }else{
+        data.push([open,close,lowest,highest])
+        open = element.recordedValue
+        close = element.recordedValue
+        lowest = element.recordedValue
+        highest = element.recordedValue
+        dates.push([currentDate.getFullYear(), currentDate.getMonth() + 1, currentDate.getDate()].join('/'))
+        currentDate = new Date(element.recordedAt)
+      }
+    });
+
+    console.log(data)
+    this.chartOption = {
+      tooltip: {
+        trigger: 'axis',
+            axisPointer: {
+                type: 'cross'
+            },
+            borderWidth: 1,
+            borderColor: '#ccc',
+            padding: 10,
+            textStyle: {
+                color: '#000'
+            },
+    
+    },
+      toolbox: {
+        feature: {
+            dataZoom: {
+                yAxisIndex: 'none'
+            },
+            restore: {},
+            saveAsImage: {}
+        }
+    },
+      xAxis: {
+        type: 'category',
+        boundaryGap: false,
+        data: dates
+      },
+      yAxis: {
+        type: 'value',
+        boundaryGap: [0, '100%']
+      },
+      dataZoom: [{
+        type: 'inside',
+        filterMode: 'filter'
+        
+    }, {
+        filterMode: 'empty'
+    }],
+      series: [
+        {
+          name: this.historicalData.share.shareName,
+          data: data,
+          type: 'candlestick',
+          
+        },
+      ],
+    };
+  }
+
+
+  /**
+   * This is for the small views of the chart, where not much detail is needed
+   * public createChart(): void {
+    console.log(this.historicalData);
+
+    let x: Array<Date> = [];
+    let y: Array<number> = [];
+    let data: Array<any> = [];
 
     //this sorts the data. should not be necessary, would be best if the backend does this
     this.historicalData.chartValues = this.historicalData.chartValues.sort((n1,n2) => {
@@ -116,7 +219,7 @@ export class ShareComponent implements OnInit {
       ],
     };
   }
-
+   */
 
 
 }
