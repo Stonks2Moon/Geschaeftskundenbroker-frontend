f6a0a708b043d52cbacb3c9b42f600afa10ca9d9
Tony1704 <anton.ochel1@gmail.com>
2021-03-11 11:11:57 +0100

    chart modified

--- frontend/src/app/components/share/share.component.ts
+++ frontend/src/app/components/share/share.component.ts
@@ -1,6 +1,7 @@
 import { AfterContentInit, AfterViewInit, Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { EChartsOption } from 'echarts';
+import { min } from 'rxjs/operators';
 import { HistoricalData, Share } from 'src/app/logic/data-models/data-models';
 import { ShareService } from 'src/app/logic/services/share.service';
 
@@ -39,29 +40,77 @@ export class ShareComponent implements OnInit {
   }
 
   public createChart(): void {
-    console.log('test');
     console.log(this.historicalData);
 
-    let x: Array<string> = [];
+    let x: Array<Date> = [];
     let y: Array<number> = [];
+    let data: Array<any> = [];
+
+    //this sorts the data. should not be necessary, would be best if the backend does this
+    this.historicalData.chartValues = this.historicalData.chartValues.sort((n1,n2) => {
+      if (n1.recordedAt > n2.recordedAt) {
+          return 1;
+      }
+  
+      if (n1.recordedAt < n2.recordedAt) {
+          return -1;
+      }
+  
+      return 0;
+  });
 
     this.historicalData?.chartValues.forEach(element => {
-      x.push(element.recordedAt.toString());
+      x.push(element.recordedAt);
       y.push(element.recordedValue);
+      data.push([element.recordedAt ,element.recordedValue])
     });
 
-
+    console.log(data)
     this.chartOption = {
+      tooltip: {
+        trigger: 'axis',
+            axisPointer: {
+                type: 'cross'
+            },
+            borderWidth: 1,
+            borderColor: '#ccc',
+            padding: 10,
+            textStyle: {
+                color: '#000'
+            },
+    
+    },
+      toolbox: {
+        feature: {
+            dataZoom: {
+                yAxisIndex: 'none'
+            },
+            restore: {},
+            saveAsImage: {}
+        }
+    },
       xAxis: {
-        type: 'category',
-        data: x,
+        type: 'time',
+        boundaryGap: false,
       },
       yAxis: {
         type: 'value',
+        boundaryGap: [0, '100%']
       },
+      dataZoom: [{
+        type: 'inside',
+        filterMode: 'filter'
+        
+    }, {
+        filterMode: 'empty'
+    }],
       series: [
         {
-          data: y,
+          name: this.historicalData.share.shareName,
+          data: data,
+          smooth: false,
+          symbol: 'none',
+          areaStyle: {},
           type: 'line',
         },
       ],
