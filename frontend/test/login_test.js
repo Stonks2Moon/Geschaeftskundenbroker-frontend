6d955069dff9ea40a3e8a0c1b730aa99d6bba8ac
tizian123 <pizzajojomgh@gmail.com>
2021-04-07 19:35:47 +0200

    login_test erweitert

--- frontend/test/login_test.js
+++ frontend/test/login_test.js
@@ -8,6 +8,24 @@ Scenario('Ich teste das Einloggen', ({ Ich }) => {
     Ich.fülle_das_feld('E-Mail', 'jojo@mail.com');
     Ich.fülle_das_feld('Passwort', '1234');
     Ich.klicke('Anmelden');
+    // pause();
+
 });
 
+Scenario('Ich teste das Einloggen', ({ Ich }) => {
+    Ich.fülle_das_feld('E-Mail', 'jojo@mail.com');
+    Ich.fülle_das_feld('Passwort', '12345');
+    Ich.klicke('Anmelden');
+    Ich.sehe('Anmeldung fehlgeschlagen!');
+    // pause();
+  
+});
+
+Scenario('Ich teste das Einloggen', ({ Ich }) => {
+    Ich.fülle_das_feld('E-Mail', 'jojo@maila.com');
+    Ich.fülle_das_feld('Passwort', '12345');
+    Ich.klicke('Anmelden');
+    Ich.sehe('Anmeldung fehlgeschlagen!');
+    // pause();
+});
 
